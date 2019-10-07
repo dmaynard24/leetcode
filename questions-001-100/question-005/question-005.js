8f301846d735e72e1a96b77e75aac4358c404ee4
dmaynardsp <dmaynard@sagepath.com>
2019-10-06 22:55:44 -0400

    solution 5: longest palindromic substring

---
+++ questions-001-100/question-005/question-005.js
@@ -0,0 +1,68 @@
+// Longest Palindromic Substring
+
+// Problem 5
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+// Example 1:
+// Input: "babad"
+// Output: "bab"
+// Note: "aba" is also a valid answer.
+
+// Example 2:
+// Input: "cbbd"
+// Output: "bb"
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  if (!s || s.length == 1) {
+    return s;
+  }
+
+  let longestLength = 1,
+    longest = s.substring(0, 1);
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.length - i < longestLength / 2 + 1) {
+      break;
+    }
+
+    let prev = s[i - 1],
+      curr = s[i],
+      next = s[i + 1];
+
+    if (curr == next) {
+      checkPalindrome(2, i - 1, i + 2);
+    }
+    if (prev && prev == next) {
+      checkPalindrome(3, i - 2, i + 2);
+    }
+  }
+
+  function checkPalindrome(length, prevI, nextI) {
+    let prev = s[prevI],
+      next = s[nextI],
+      j = 0;
+    while (prev && next && prev == next) {
+      length += 2;
+      j++;
+      prev = s[prevI - j];
+      next = s[nextI + j];
+    }
+
+    if (length > longestLength) {
+      longest = s.substring(prevI - j + 1, nextI + j);
+      longestLength = length;
+    }
+  }
+
+  return longest;
+};
+
+const now = require('performance-now');
+const time0 = now();
+console.log(longestPalindrome('aaaa'));
+const time1 = now();
+console.log(`call took ${time1 - time0} milliseconds`);
