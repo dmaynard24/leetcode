6a9fd47c67d2bb584d47423e7684ac8ebc262cf9
dmaynardsp <dmaynard@sagepath.com>
2019-10-07 20:49:43 -0400

    solution 7 - reverse integer

---
+++ questions-001-100/question-007/question-007.js
@@ -0,0 +1,46 @@
+// Reverse Integer
+
+// Problem 7
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Example 1:
+// Input: 123
+// Output: 321
+
+// Example 2:
+// Input: -123
+// Output: -321
+
+// Example 3:
+// Input: 120
+// Output: 21
+
+// Note:
+// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  let isNegative = false;
+
+  if (x < 0) {
+    isNegative = true;
+    x *= -1;
+  }
+
+  let reversed = 0;
+  while (x > 0) {
+    reversed = reversed * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  let num = isNegative ? reversed * -1 : reversed;
+
+  let MAX_32BIT_INT = Math.pow(2, 31) - 1,
+    MIN_32BIT_INT = MAX_32BIT_INT * -1;
+  return num > MAX_32BIT_INT || num < MIN_32BIT_INT ? 0 : num;
+};
+
+module.exports = reverse;
