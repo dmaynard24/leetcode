0ac0ddb78024edabc18990bf496532ecf8a3c37f
dmaynardsp <dmaynard@sagepath.com>
2019-10-07 23:01:15 -0400

    solution 9. faster than 94% of JavaScript answers, less memory usage than 100% of JavaScript answers :)

---
+++ questions-001-100/question-009/question-009.js
@@ -0,0 +1,63 @@
+// Palindrome Number
+
+// Problem 9
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+// Example 1:
+// Input: 121
+// Output: true
+
+// Example 2:
+// Input: -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+// Example 3:
+// Input: 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+// Follow up:
+
+// Could you solve it without converting the integer to a string?
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x < 0) {
+    return false;
+  }
+
+  if (x < 10) {
+    return true;
+  }
+
+  let digitsReversed = [];
+  while (x > 0) {
+    digitsReversed.push(x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  if (digitsReversed[0] == 0) {
+    return false;
+  }
+
+  let lIndex = 0,
+    rIndex = digitsReversed.length - 1;
+  while (rIndex - lIndex > 0) {
+    l = digitsReversed[lIndex];
+    r = digitsReversed[rIndex];
+
+    if (l != r) {
+      return false;
+    }
+
+    lIndex++;
+    rIndex--;
+  }
+
+  return true;
+};
+
+module.exports = isPalindrome;
---
+++ questions-001-100/question-009/question-009.test.js
@@ -0,0 +1,13 @@
+const isPalindrome = require('./question-009');
+
+test('expects that 121 is a palindrome', () => {
+  expect(isPalindrome(121)).toBe(true);
+});
+
+test('expects that -121 is a palindrome', () => {
+  expect(isPalindrome(-121)).toBe(false);
+});
+
+test('expects that 10 is a palindrome', () => {
+  expect(isPalindrome(10)).toBe(false);
+});
