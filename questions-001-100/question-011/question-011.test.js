5c3341fb9ef40acec20f935302ab882919610502
Dave Maynard <davemaynard24@gmail.com>
2019-10-10 18:44:51 -0400

    naive solution for question 11

---
+++ questions-001-100/question-011/question-011.test.js
@@ -0,0 +1,9 @@
+const maxArea = require('./question-011');
+
+test('gets the maximum area of vertical lines represented by [1, 8, 6, 2, 5, 4, 8, 3, 7] to be 49', () => {
+  expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+});
+
+test('gets the maximum area of vertical lines represented by [1, 1] to be 1', () => {
+  expect(maxArea([1, 1])).toBe(1);
+});
