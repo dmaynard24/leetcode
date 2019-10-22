69635d2c0e373c37f98ff1beb72ad9a574ee1469
dmaynardsp <dmaynard@sagepath.com>
2019-10-22 00:55:40 -0400

    solution 17 with recursion

---
+++ questions-001-100/question-017/question-017.js
@@ -0,0 +1,57 @@
+// Letter Combinations of a Phone Number
+
+// Problem 17
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+// Example:
+// Input: "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+// Note:
+// Although the above answer is in lexicographical order, your answer could be in any order you want.
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  if (digits.length == 0) {
+    return [];
+  }
+
+  const phoneNumberLetters = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+  };
+
+  let digitsArr = digits.split('');
+
+  function getCombos(index, combo) {
+    let combos = [];
+
+    if (index == digitsArr.length) {
+      combos = combos.concat(combo);
+    } else {
+      let digitLetters = phoneNumberLetters[digitsArr[index]];
+
+      for (let i = 0; i < digitLetters.length; i++) {
+        let newCombo = combo + digitLetters[i];
+        combos = combos.concat(getCombos(index + 1, newCombo));
+      }
+    }
+
+    return combos;
+  }
+
+  return getCombos(0, '');
+};
+
+module.exports = letterCombinations;
---
+++ questions-001-100/question-017/question-017.test.js
@@ -0,0 +1,5 @@
+const letterCombinations = require('./question-017');
+
+test('gets all possible letter combinations that the string "23" could represent in a phone number to be ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]', () => {
+  expect(letterCombinations('23')).toStrictEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']);
+});
