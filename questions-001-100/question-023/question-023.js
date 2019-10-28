aa7bf23479c7b36e004fbd35643253af806734c5
Dave Maynard <dmaynard@sagepath.com>
2019-10-28 18:34:49 -0400

    solution 23

---
+++ questions-001-100/question-023/question-023.js
@@ -0,0 +1,70 @@
+// Merge k Sorted Lists
+
+// Problem 23
+// Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+// Example:
+// Input:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// Output: 1->1->2->3->4->4->5->6
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+  let returnNode = new ListNode(0),
+    node = returnNode;
+
+  let nonNulls = lists.filter(list => list != null);
+  while (nonNulls.length > 1) {
+    let smallestListVal = Infinity,
+      smallestListIndex;
+
+    for (let i = 0; i < nonNulls.length; i++) {
+      if (nonNulls[i].val < smallestListVal) {
+        smallestListVal = nonNulls[i].val;
+        smallestListIndex = i;
+      }
+    }
+
+    node.next = new ListNode(nonNulls[smallestListIndex].val);
+    nonNulls[smallestListIndex] = advanceList(nonNulls[smallestListIndex]);
+    node = node.next;
+
+    if (nonNulls[smallestListIndex] == null) {
+      nonNulls.splice(smallestListIndex, 1);
+    }
+  }
+
+  let lastList = nonNulls[0];
+  while (lastList != null) {
+    node.next = new ListNode(lastList.val);
+    lastList = advanceList(lastList);
+    node = node.next;
+  }
+
+  return returnNode.next;
+};
+
+var advanceList = function(list) {
+  return list && list.next ? list.next : null;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+module.exports = { ListNode, mergeKLists };
---
+++ questions-001-100/question-023/question-023.test.js
@@ -0,0 +1,25 @@
+const q = require('./question-023');
+
+let l1 = new q.ListNode(1);
+l1.next = new q.ListNode(4);
+l1.next.next = new q.ListNode(5);
+
+let l2 = new q.ListNode(1);
+l2.next = new q.ListNode(3);
+l2.next.next = new q.ListNode(4);
+
+let l3 = new q.ListNode(2);
+l3.next = new q.ListNode(6);
+
+let solutionNode = new q.ListNode(1);
+solutionNode.next = new q.ListNode(1);
+solutionNode.next.next = new q.ListNode(2);
+solutionNode.next.next.next = new q.ListNode(3);
+solutionNode.next.next.next.next = new q.ListNode(4);
+solutionNode.next.next.next.next.next = new q.ListNode(4);
+solutionNode.next.next.next.next.next.next = new q.ListNode(5);
+solutionNode.next.next.next.next.next.next.next = new q.ListNode(6);
+
+test('expects that merging the lists [(1 -> 4 -> 5), (1 -> 3 -> 4), (2 -> 6)] will result in (1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6)', () => {
+  expect(q.mergeKLists([l1, l2, l3])).toStrictEqual(solutionNode);
+});
