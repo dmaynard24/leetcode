1c3e6b06fbac285863084b1bdbba9bb031414477
dmaynardsp <dmaynard@sagepath.com>
2019-10-29 17:16:02 -0400

    optimized solution 21 with recursion

--- questions-001-100/question-021/question-021.js
+++ questions-001-100/question-021/question-021.js
@@ -13,39 +13,16 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-  let returnNode = new ListNode(0),
-    node = returnNode;
+  if (l1 == null) return l2;
+  if (l2 == null) return l1;
 
-  while (l1 != null && l2 != null) {
-    if (l1.val < l2.val) {
-      node.next = new ListNode(l1.val);
-      l1 = advanceList(l1);
-    } else {
-      node.next = new ListNode(l2.val);
-      l2 = advanceList(l2);
-    }
-    node = node.next;
-  }
-
-  if (l1 != null) {
-    while (l1 != null) {
-      node.next = new ListNode(l1.val);
-      l1 = advanceList(l1);
-      node = node.next;
-    }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
   } else {
-    while (l2 != null) {
-      node.next = new ListNode(l2.val);
-      l2 = advanceList(l2);
-      node = node.next;
-    }
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
   }
-
-  return returnNode.next;
-};
-
-var advanceList = function(list) {
-  return list && list.next ? list.next : null;
 };
 
 /**
