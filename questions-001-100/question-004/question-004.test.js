6a96bd401889d36968deca20732debb4391a8bc0
dmaynard24 <davemaynard24@gmail.com>
2019-10-30 19:34:00 -0400

    added a test case to question 4 before attempting to optimize it with a binary search

--- questions-001-100/question-004/question-004.test.js
+++ questions-001-100/question-004/question-004.test.js
@@ -7,3 +7,7 @@ test('gets the median of two sorted arrays [1, 3] and [2] to be 2', () => {
 test('gets the median of two sorted arrays [1, 2] and [3, 4] to be 2.5', () => {
   expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
 });
+
+test('gets the median of two sorted arrays [1, 3, 8, 9, 15] and [7, 11, 18, 19, 21, 25] to be 11', () => {
+  expect(findMedianSortedArrays([1, 3, 8, 9, 15], [7, 11, 18, 19, 21, 25])).toBe(11);
+});
