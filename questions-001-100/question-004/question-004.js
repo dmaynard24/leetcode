92c2f9ec1d8985df5739be5a066f3d536d325811
dmaynard24 <davemaynard24@gmail.com>
2019-10-31 11:29:44 -0400

    still working through a binary search for solution 4

--- questions-001-100/question-004/question-004.js
+++ questions-001-100/question-004/question-004.js
@@ -24,11 +24,43 @@
  * @param {number[]} nums2
  * @return {number}
  */
+// var findMedianSortedArrays = function(nums1, nums2) {
+//   // attempt to concat in order based on the first term of each array
+//   let both = nums1.concat(nums2).sort((a, b) => a - b),
+//     len = both.length;
+//   return len % 2 == 1 ? both[Math.floor(len / 2)] : (both[len / 2 - 1] + both[len / 2]) / 2;
+// };
+
 var findMedianSortedArrays = function(nums1, nums2) {
-  // attempt to concat in order based on the first term of each array
-  let both = nums1.concat(nums2).sort((a, b) => a - b),
-    len = both.length;
-  return len % 2 == 1 ? both[Math.floor(len / 2)] : (both[len / 2 - 1] + both[len / 2]) / 2;
+  if (nums1.length > nums2.length) {
+    findMedianSortedArrays(nums2, nums1);
+  }
+
+  let xLength = nums1.length,
+    yLength = nums2.length,
+    low = 0,
+    high = xLength;
+  while (low <= high) {
+    let partitionX = Math.floor((low + high) / 2),
+      partitionY = (xLength + yLength + 1) / 2 - partitionX,
+      lowX = nums1[partitionX - 1] ? nums1[partitionX - 1] : Number.MIN_SAFE_INTEGER,
+      highX = nums1[partitionX] ? nums1[partitionX] : Number.MAX_SAFE_INTEGER,
+      lowY = nums2[partitionY - 1] ? nums2[partitionY - 1] : Number.MIN_SAFE_INTEGER,
+      highY = nums2[partitionY] ? nums2[partitionY] : Number.MAX_SAFE_INTEGER;
+
+    if (lowX <= highY && lowY <= highX) {
+      if ((xLength + yLength) % 2 == 0) {
+        // console.log('HERE!', lowX, lowY, highX, highY);
+        return (Math.max(lowX, lowY) + Math.min(highX, highY)) / 2;
+      } else {
+        return Math.max(lowX, lowY);
+      }
+    } else if (lowX > highY) {
+      high = partitionX - 1;
+    } else {
+      low = partitionX + 1;
+    }
+  }
 };
 
 module.exports = findMedianSortedArrays;
