2dc237869d4b5f442fb880de0dab4b243b0c982d
dmaynard24 <davemaynard24@gmail.com>
2019-11-01 11:21:43 -0400

    naive solution 28 using indexOf

---
+++ questions-001-100/question-028/question-028.js
@@ -0,0 +1,25 @@
+// Implement strStr()
+
+// Problem 28
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+
+// Example 2:
+// Input: haystack = "aaaaa", needle = "bba"
+// Output: -1
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+module.exports = strStr;
---
+++ questions-001-100/question-028/question-028.test.js
@@ -0,0 +1,9 @@
+const strStr = require('./question-028');
+
+test('gets the first index of the needle "ll" in the haystack "hello" to be 2', () => {
+  expect(strStr('hello', 'll')).toBe(2);
+});
+
+test('gets the first index of the needle "bba" in the haystack "aaaaa" to be -1 since the needle is not in the haystack', () => {
+  expect(strStr('aaaaa', 'bba')).toBe(-1);
+});
