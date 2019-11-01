a49a51c9f13a97557c0e479a02f5877ae75ae576
dmaynard24 <davemaynard24@gmail.com>
2019-11-01 11:13:28 -0400

    optimized solution 27 by only splicing once

--- questions-001-100/question-027/question-027.js
+++ questions-001-100/question-027/question-027.js
@@ -23,12 +23,15 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] == val) {
-      nums.splice(i, 1);
-      i--;
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] != val) {
+      nums[i] = nums[j];
+      i++;
     }
   }
+
+  nums.splice(i);
 
   return nums.length;
 };
