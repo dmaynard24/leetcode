8db60bcd6875434fb4d2dc7dbd618a9d5f0025f7
dmaynard24 <davemaynard24@gmail.com>
2019-11-06 17:45:06 -0500

    slightly optimized solution 5 by reducing complexity for odd/even lengh substrings

--- questions-001-100/question-005/question-005.js
+++ questions-001-100/question-005/question-005.js
@@ -25,38 +25,29 @@ var longestPalindrome = function(s) {
     longestEnd = 1,
     longestLength = 1;
 
-  for (let i = 0; i < s.length; i++) {
+  for (let i = 0; i < s.length - 1; i++) {
     if (s.length - i < longestLength / 2 + 1) {
       break;
     }
 
-    let prev = s[i - 1],
-      curr = s[i],
-      next = s[i + 1];
+    for (let j = i; j <= i + 1; j++) {
+      let prev = s[i],
+        next = s[j],
+        l = 0;
+      while (prev && next && prev == next) {
+        l++;
+        prev = s[i - l];
+        next = s[j + l];
+      }
 
-    if (curr == next) {
-      checkPalindrome(2, i - 1, i + 2);
-    }
-    if (prev && prev == next) {
-      checkPalindrome(3, i - 2, i + 2);
-    }
-  }
-
-  function checkPalindrome(length, prevI, nextI) {
-    let prev = s[prevI],
-      next = s[nextI],
-      j = 0;
-    while (prev && next && prev == next) {
-      length += 2;
-      j++;
-      prev = s[prevI - j];
-      next = s[nextI + j];
-    }
-
-    if (length > longestLength) {
-      longest = longestStart = prevI - j + 1;
-      longestEnd = nextI + j;
-      longestLength = length;
+      let start = i - l + 1,
+        end = j + l,
+        length = end - start;
+      if (length > longestLength) {
+        longestStart = start;
+        longestEnd = end;
+        longestLength = length;
+      }
     }
   }
 
