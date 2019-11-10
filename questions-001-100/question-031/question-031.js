750bfb7b1013b84971da73710fc9be0a5c3c4564
dmaynardsp <dmaynard@sagepath.com>
2019-11-10 16:09:39 -0500

    finally solved question 31

--- questions-001-100/question-031/question-031.js
+++ questions-001-100/question-031/question-031.js
@@ -18,30 +18,58 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-  let isLargestPerm = true;
+  // move from left to right looking for a digit that's larger than the previous.
+  // continue looking, don't break out.
+  // slice everything starting from the left digit. sort it ascending.
+  // "unshift" the slice with the first val that's larger than previous, and concat it.
+
+  let smallerI;
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] < nums[i]) {
-      isLargestPerm = false;
-      break;
+    let prev = nums[i - 1],
+      curr = nums[i];
+
+    if (curr > prev) {
+      smallerI = i - 1;
     }
   }
 
-  if (isLargestPerm) {
+  if (smallerI === undefined) {
     return nums.reverse();
   }
 
-  let len = nums.length;
-  for (let offset = 0; offset < len - 1; offset++) {
-    let first = len - 2 - offset,
-      second = len - 1 - offset;
+  let arr;
 
-    // swap with destructuring assignment
-    [nums[first], nums[second]] = [nums[second], nums[first]];
-
-    if (nums[first] > nums[second]) {
-      break;
+  if (smallerI !== 0) {
+    let smallerVal = nums[smallerI],
+      sortedSlice = nums.slice(smallerI).sort((a, b) => a - b),
+      firstLargerI;
+    for (let i = 0; i < sortedSlice.length; i++) {
+      if (sortedSlice[i] > smallerVal) {
+        firstLargerI = i;
+        break;
+      }
     }
+
+    arr = nums
+      .slice(0, smallerI)
+      .concat(sortedSlice.splice(firstLargerI, 1))
+      .concat(sortedSlice);
+  } else {
+    // edge case: if left digit's index is 0, slice everything staring from the first digit that's larger than nums[0] and unshift nums with it. sort the rest ascending.
+    let firstLargerI;
+    for (let i = nums.length - 1; i > 0; i--) {
+      if (nums[i] > nums[0]) {
+        firstLargerI = i;
+        break;
+      }
+    }
+
+    arr = nums.slice(firstLargerI).concat(nums.slice(0, firstLargerI).sort((a, b) => a - b));
   }
+
+  arr.forEach((num, i) => {
+    nums[i] = num;
+  });
 
   return nums;
 };
--- questions-001-100/question-031/question-031.test.js
+++ questions-001-100/question-031/question-031.test.js
@@ -8,10 +8,6 @@ test('expects the next lexicographic permutation of [3, 2, 1] to be [1, 2, 3]', 
   expect(nextPermutation([3, 2, 1])).toStrictEqual([1, 2, 3]);
 });
 
-test('expects the next lexicographic permutation of [1, 1, 5] to be [1, 5, 1]', () => {
-  expect(nextPermutation([1, 1, 5])).toStrictEqual([1, 5, 1]);
-});
-
 test('expects the next lexicographic permutation of [1, 2] to be [2, 1]', () => {
   expect(nextPermutation([1, 2])).toStrictEqual([2, 1]);
 });
@@ -19,3 +15,19 @@ test('expects the next lexicographic permutation of [1, 2] to be [2, 1]', () => 
 test('expects the next lexicographic permutation of [1, 3, 2] to be [2, 1, 3]', () => {
   expect(nextPermutation([1, 3, 2])).toStrictEqual([2, 1, 3]);
 });
+
+test('expects the next lexicographic permutation of [5, 4, 7, 5, 3, 2] to be [5, 5, 2, 3, 4, 7]', () => {
+  expect(nextPermutation([5, 4, 7, 5, 3, 2])).toStrictEqual([5, 5, 2, 3, 4, 7]);
+});
+
+test('expects the next lexicographic permutation of [2, 3, 1] to be [3, 1, 2]', () => {
+  expect(nextPermutation([2, 3, 1])).toStrictEqual([3, 1, 2]);
+});
+
+test('expects the next lexicographic permutation of [1, 4, 3, 2, 2] to be [2, 1, 2, 3, 4]', () => {
+  expect(nextPermutation([1, 4, 3, 2, 2])).toStrictEqual([2, 1, 2, 3, 4]);
+});
+
+// move from left to right looking for a digit that's larger than the previous. slice everything starting from the left digit, sort it ascending and concat it.
+
+// edge case: if left digit's index is 0, slice everything staring from the first digit that's larger than nums[0] and unshift nums with it.
