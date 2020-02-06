7c1c381e71d8839b6e0693e2093a9d13017f085f
dmaynard24 <davemaynard24@gmail.com>
2020-02-06 13:46:31 -0500

    ported solution 9 to python

---
+++ python/questions_001_100/question_009/is_palindrome_test.py
@@ -0,0 +1,12 @@
+import unittest, is_palindrome
+
+
+class TestIsPalindrome(unittest.TestCase):
+  def test_is_palindrome(self):
+    self.assertEqual(is_palindrome.Solution().is_palindrome(121), True)
+    self.assertEqual(is_palindrome.Solution().is_palindrome(-121), False)
+    self.assertEqual(is_palindrome.Solution().is_palindrome(10), False)
+
+
+if __name__ == '__main__':
+  unittest.main()
