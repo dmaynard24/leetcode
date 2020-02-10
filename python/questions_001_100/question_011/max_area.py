5390af4c924ba5f5b7d4800943052ad383668359
dmaynard24 <davemaynard24@gmail.com>
2020-02-10 15:36:48 -0500

    ported solution 11 to python

---
+++ python/questions_001_100/question_011/max_area.py
@@ -0,0 +1,35 @@
+# Container With Most Water
+
+# Problem 11
+# Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+# Note: You may not slant the container and n is at least 2.
+
+# The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+# Example:
+# Input: [1,8,6,2,5,4,8,3,7]
+# Output: 49
+
+
+class Solution:
+  def max_area(self, height: [int]) -> int:
+    l = 0
+    r = len(height) - 1
+    largest_area = 0
+
+    while l < r:
+      left_height = height[l]
+      right_height = height[r]
+      w = r + 1 - (l + 1)
+      h = min(left_height, right_height)
+      area = w * h
+
+      largest_area = max(largest_area, area)
+
+      if left_height < right_height:
+        l += 1
+      else:
+        r -= 1
+
+    return largest_area
---
+++ python/questions_001_100/question_011/max_area_test.py
@@ -0,0 +1,15 @@
+import unittest, max_area
+
+
+class TestMaxArea(unittest.TestCase):
+  def test_max_area(self):
+    self.assertEqual(max_area.Solution().max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]),
+                     49)
+    self.assertEqual(max_area.Solution().max_area([1, 1]), 1)
+    self.assertEqual(max_area.Solution().max_area([1, 2, 1]), 2)
+    self.assertEqual(
+        max_area.Solution().max_area([9, 6, 14, 11, 2, 2, 4, 9, 3, 8]), 72)
+
+
+if __name__ == '__main__':
+  unittest.main()
