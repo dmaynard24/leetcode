b23cdb7a66486bab6c1f693d41fa6df7369fd4d8
dmaynard24 <davemaynard24@gmail.com>
2020-02-11 11:30:48 -0500

    ported solution 12 to python

---
+++ python/questions_001_100/question_012/int_to_roman_test.py
@@ -0,0 +1,14 @@
+import unittest, int_to_roman
+
+
+class TestMaxArea(unittest.TestCase):
+  def test_int_to_roman(self):
+    self.assertEqual(int_to_roman.Solution().int_to_roman(3), 'III')
+    self.assertEqual(int_to_roman.Solution().int_to_roman(4), 'IV')
+    self.assertEqual(int_to_roman.Solution().int_to_roman(9), 'IX')
+    self.assertEqual(int_to_roman.Solution().int_to_roman(58), 'LVIII')
+    self.assertEqual(int_to_roman.Solution().int_to_roman(1994), 'MCMXCIV')
+
+
+if __name__ == '__main__':
+  unittest.main()
