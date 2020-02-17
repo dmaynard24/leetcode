ff713e5c6db3f4577038d49533a9ca23dd82387b
dmaynard24 <davemaynard24@gmail.com>
2020-02-17 17:23:33 -0500

    ported solution 19 to python

---
+++ python/questions_001_100/question_019/remove_nth_from_end.py
@@ -0,0 +1,40 @@
+# Remove Nth Node From End of List
+
+# Problem 19
+# Given a linked list, remove the n-th node from the end of list and return its head.
+
+# Example:
+
+# Given linked list: 1->2->3->4->5, and n = 2.
+
+# After removing the second node from the end, the linked list becomes 1->2->3->5.
+# Note:
+
+# Given n will always be valid.
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import listnode
+
+
+class Solution:
+  def remove_nth_from_end(self, head: listnode.ListNode,
+                          n: int) -> listnode.ListNode:
+    return_node = listnode.ListNode(0)
+    return_node.next = head
+
+    first = return_node
+    second = return_node
+
+    for _ in range(n + 1):
+      first = first.next
+
+    while first is not None:
+      first = first.next
+      second = second.next
+
+    second.next = second.next.next
+
+    return return_node.next
---
+++ python/questions_001_100/question_019/remove_nth_from_end_test.py
@@ -0,0 +1,32 @@
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import listnode
+
+import unittest, remove_nth_from_end
+
+l1 = listnode.ListNode(1)
+l1.next = listnode.ListNode(2)
+l1.next.next = listnode.ListNode(3)
+l1.next.next.next = listnode.ListNode(4)
+l1.next.next.next.next = listnode.ListNode(5)
+
+
+class TestRemoveNthFromEnd(unittest.TestCase):
+  def test_remove_nth_from_end(self):
+    solution_node_expected_vals = [1, 2, 3, 5]
+    solution_node = remove_nth_from_end.Solution().remove_nth_from_end(l1, 2)
+    solution_node_vals = []
+
+    while solution_node.next is not None:
+      solution_node_vals.append(solution_node.val)
+      solution_node = solution_node.next
+
+    solution_node_vals.append(solution_node.val)
+
+    self.assertListEqual(solution_node_expected_vals, solution_node_vals)
+
+
+if __name__ == '__main__':
+  unittest.main()
