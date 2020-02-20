af014415c56aa054631b32c6ddcbe332c7cc90e8
dmaynardsp <dmaynard@sagepath.com>
2020-02-19 22:43:36 -0500

    ported solution 23 to python

---
+++ python/questions_001_100/question_023/merge_k_lists.py
@@ -0,0 +1,38 @@
+# Merge k Sorted Lists
+
+# Problem 23
+# Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+# Example:
+# Input:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# Output: 1->1->2->3->4->4->5->6
+
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import listnode
+from python.questions_001_100.question_021 import merge_two_lists
+merge_two_lists = merge_two_lists.Solution().merge_two_lists
+
+
+class Solution:
+  def merge_k_lists(self, lists: [listnode.ListNode]) -> listnode.ListNode:
+    if len(lists) == 0:
+      return None
+
+    while len(lists) > 1:
+      offset = 1 if len(lists) % 2 == 0 else 2
+      for i in range(0, len(lists) - offset, 2):
+        if len(lists) < i + 2:
+          break
+        lists.insert(i, merge_two_lists(lists[i], lists[i + 1]))
+        lists.pop(i + 1)
+        lists.pop(i + 1)
+
+    return lists[0]
---
+++ python/questions_001_100/question_023/merge_k_lists_test.py
@@ -0,0 +1,37 @@
+import sys, os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.append(root_dir)
+
+from python.util import listnode
+
+import unittest, merge_k_lists
+
+l1 = listnode.ListNode(1)
+l1.next = listnode.ListNode(4)
+l1.next.next = listnode.ListNode(5)
+
+l2 = listnode.ListNode(1)
+l2.next = listnode.ListNode(3)
+l2.next.next = listnode.ListNode(4)
+
+l3 = listnode.ListNode(2)
+l3.next = listnode.ListNode(6)
+
+
+class TestMergeKLists(unittest.TestCase):
+  def test_merge_k_lists(self):
+    solution_node_expected_vals = [1, 1, 2, 3, 4, 4, 5, 6]
+    solution_node = merge_k_lists.Solution().merge_k_lists([l1, l2, l3])
+    solution_node_vals = []
+
+    while solution_node.next is not None:
+      solution_node_vals.append(solution_node.val)
+      solution_node = solution_node.next
+
+    solution_node_vals.append(solution_node.val)
+
+    self.assertListEqual(solution_node_expected_vals, solution_node_vals)
+
+
+if __name__ == '__main__':
+  unittest.main()
