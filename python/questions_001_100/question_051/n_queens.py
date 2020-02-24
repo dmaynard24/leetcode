7ecc506f989ff5804969eb2546b6fe66acfdb38e
dmaynard24 <davemaynard24@gmail.com>
2020-02-24 11:03:19 -0500

    updated solution 51 for readability and to follow a more conventional back-tracking algorithm

--- python/questions_001_100/question_051/n_queens.py
+++ python/questions_001_100/question_051/n_queens.py
@@ -1,28 +1,28 @@
-# # N-Queens
+# N-Queens
 
-# # Problem 51
-# # The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+# Problem 51
+# The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
 
-# # Given an integer n, return all distinct solutions to the n-queens puzzle.
+# Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-# # Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+# Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
 
-# # Example
+# Example
 
-# # Input: 4
-# # Output: [
-# #  [".Q..",  // Solution 1
-# #   "...Q",
-# #   "Q...",
-# #   "..Q."],
+# Input: 4
+# Output: [
+#  [".Q..",  // Solution 1
+#   "...Q",
+#   "Q...",
+#   "..Q."],
 
-# #  ["..Q.",  // Solution 2
-# #   "Q...",
-# #   "...Q",
-# #   ".Q.."]
-# # ]
+#  ["..Q.",  // Solution 2
+#   "Q...",
+#   "...Q",
+#   ".Q.."]
+# ]
 
-# # Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+# Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
 
 
 class Solution:
@@ -39,29 +39,44 @@ class Solution:
           for coord in coords
       ]
 
-    def place_next_queen(placed_x_coords, xy_diffs, xy_sums, solution_set):
+    def place_next_queen(x, placed_x_coords, xy_diffs, xy_sums, solution_set):
       y = len(placed_x_coords)
+
+      # base case
+      if y == 0:
+        if has_even_extent:
+          if x == n // 2:
+            return solution_set
+        else:
+          if x == n // 2 + 1:
+            return solution_set
 
       if y == n:
         if has_even_extent or (has_even_extent == False
                                and placed_x_coords[0] < n // 2):
-          reflected_x_coords = [n - coord - 1 for coord in placed_x_coords]
-          solution_set.append(get_board_from_coords(reflected_x_coords))
+          reflected_coords = [n - coord - 1 for coord in placed_x_coords]
+          solution_set.append(get_board_from_coords(reflected_coords))
         solution_set.append(get_board_from_coords(placed_x_coords))
 
-      for x in range(n):
-        xy_diff = y - x
-        xy_sum = y + x
-        if x not in placed_x_coords and xy_diff not in xy_diffs and xy_sum not in xy_sums:
-          # base case
-          if y == 0:
-            if has_even_extent:
-              if x == n // 2:
-                return solution_set
-            else:
-              if x == n // 2 + 1:
-                return solution_set
-          place_next_queen(placed_x_coords + [x], xy_diffs + [xy_diff],
-                           xy_sums + [xy_sum], solution_set)
+      for next_x in range(x, n):
+        xy_diff = y - next_x
+        xy_sum = y + next_x
+        # check if is valid place
+        if next_x not in placed_x_coords and xy_diff not in xy_diffs and xy_sum not in xy_sums:
+          placed_x_coords.append(next_x)
+          xy_diffs.append(xy_diff)
+          xy_sums.append(xy_sum)
+          return place_next_queen(0, placed_x_coords, xy_diffs, xy_sums,
+                                  solution_set)
 
-    return place_next_queen([], [], [], [])
+      if y > 0:
+        prev_placed_x_coords = placed_x_coords.pop()
+        xy_diffs.pop()
+        xy_sums.pop()
+        return place_next_queen(prev_placed_x_coords + 1, placed_x_coords,
+                                xy_diffs, xy_sums, solution_set)
+
+    return place_next_queen(0, [], [], [], [])
+
+
+print(Solution().solveNQueens(5))
