237ae7a0072494680d1884d9f56eebc7a51982fc
dmaynardsp <dmaynard@sagepath.com>
2020-02-23 23:48:41 -0500

    optimized solution 51 to get 100/100

--- python/questions_001_100/question_051/n_queens.py
+++ python/questions_001_100/question_051/n_queens.py
@@ -1,28 +1,28 @@
-# N-Queens
+# # N-Queens
 
-# Problem 51
-# The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+# # Problem 51
+# # The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
 
-# Given an integer n, return all distinct solutions to the n-queens puzzle.
+# # Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-# Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+# # Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
 
-# Example
+# # Example
 
-# Input: 4
-# Output: [
-#  [".Q..",  // Solution 1
-#   "...Q",
-#   "Q...",
-#   "..Q."],
+# # Input: 4
+# # Output: [
+# #  [".Q..",  // Solution 1
+# #   "...Q",
+# #   "Q...",
+# #   "..Q."],
 
-#  ["..Q.",  // Solution 2
-#   "Q...",
-#   "...Q",
-#   ".Q.."]
-# ]
+# #  ["..Q.",  // Solution 2
+# #   "Q...",
+# #   "...Q",
+# #   ".Q.."]
+# # ]
 
-# Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+# # Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
 
 
 class Solution:
@@ -39,42 +39,29 @@ class Solution:
           for coord in coords
       ]
 
-    def is_valid_place(test_y, test_x, placed_coords):
-      for pc_y, pc in enumerate(placed_coords):
-        # invalid vertically or invalid diagonally
-        if test_x == pc or abs(test_x - pc) == abs(test_y - pc_y):
-          return False
-      return True
+    def place_next_queen(placed_x_coords, xy_diffs, xy_sums, solution_set):
+      y = len(placed_x_coords)
 
-    def place_next_queen(y, x, placed_coords, solution_set):
-      if len(placed_coords) == 0:
-        if has_even_extent:
-          if x == n // 2:
-            return solution_set
-        else:
-          if x == n // 2 + 1:
-            return solution_set
-
-      if len(placed_coords) == n:
+      if y == n:
         if has_even_extent or (has_even_extent == False
-                               and placed_coords[0] < n // 2):
-          reflected_coords = []
-          for coord in placed_coords:
-            reflected_coords.append(n - coord - 1)
-          solution_set.append(get_board_from_coords(reflected_coords))
-        solution_set.append(get_board_from_coords(placed_coords))
+                               and placed_x_coords[0] < n // 2):
+          reflected_x_coords = [n - coord - 1 for coord in placed_x_coords]
+          solution_set.append(get_board_from_coords(reflected_x_coords))
+        solution_set.append(get_board_from_coords(placed_x_coords))
 
-      for next_x in range(x, n):
-        if is_valid_place(y, next_x, placed_coords):
-          placed_coords.append(next_x)
-          return place_next_queen(y + 1, 0, placed_coords, solution_set)
+      for x in range(n):
+        xy_diff = y - x
+        xy_sum = y + x
+        if x not in placed_x_coords and xy_diff not in xy_diffs and xy_sum not in xy_sums:
+          # base case
+          if y == 0:
+            if has_even_extent:
+              if x == n // 2:
+                return solution_set
+            else:
+              if x == n // 2 + 1:
+                return solution_set
+          place_next_queen(placed_x_coords + [x], xy_diffs + [xy_diff],
+                           xy_sums + [xy_sum], solution_set)
 
-      if len(placed_coords) > 0:
-        prev_placed_coords = placed_coords.pop()
-        return place_next_queen(y - 1, prev_placed_coords + 1, placed_coords,
-                                solution_set)
-
-    return place_next_queen(0, 0, [], [])
-
-
-print(Solution().solveNQueens(4))
+    return place_next_queen([], [], [], [])
