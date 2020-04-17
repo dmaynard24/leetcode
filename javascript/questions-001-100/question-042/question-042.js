1e452bf92874fd97cfc97db539eb0e829c191569
Dave Maynard <davemaynard24@gmail.com>
2020-04-17 17:25:52 -0400

    continued solving problem 42

--- javascript/questions-001-100/question-042/question-042.js
+++ javascript/questions-001-100/question-042/question-042.js
@@ -5,41 +5,44 @@
 const trap = function(height) {
   let amount = 0;
 
-  const peaks = new Map();
-
-  // first height
-  peaks.set(height[0], 0);
-
-  // rest of heights
+  const slopes = [height[0]];
   for (let i = 1; i < height.length; i++) {
-    if (height[i] > height[i - 1]) {
-      // up slope
-      const peaksEntries = Array.from(peaks.entries());
-      let valley = height[i];
-      for (let j = peaksEntries.length - 1; j >= 0; j--) {
-        [k, v] = peaksEntries[j];
-        const x = i - v - 1;
-        if (k < height[i]) {
-          amount += x * k;
-          valley = Math.min(valley, k);
-          peaks.delete(k);
-        } else {
-          amount += x * Math.min(k, height[i]) - valley;
-          break;
-        }
-      }
-      console.log(i, peaks.size, amount);
-    }
+    slopes.push(height[i] - height[i - 1]);
+  }
 
-    // cache peak heights
-    peaks.set(height[i], i);
+  console.log(slopes.join(`,`));
+
+  const peaks = [];
+  if (slopes[0] > slopes[1]) {
+    peaks.push(0);
+  }
+  for (let i = 1; i < slopes.length - 1; i++) {
+    if (slopes[i - 1] < slopes[i] && slopes[i] > slopes[i + 1]) {
+      peaks.push(i);
+    }
+  }
+  if (slopes[slopes.length - 2] < slopes[slopes.length - 1]) {
+    peaks.push(slopes.length - 1);
+  }
+
+  console.log(peaks);
+
+  for (let i = 0; i < peaks.length - 1; i++) {
+    const shortPeak = Math.min(height[peaks[i]], height[peaks[i + 1]]);
+    for (let j = peaks[i] + 1; j < peaks[i + 1]; j++) {
+      if (shortPeak - height[j] > 0) {
+        amount += shortPeak - height[j];
+      }
+    }
   }
 
   return amount;
 };
 
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // 6
 // console.log(trap([0, 1, 0, 2])); // 1
 // console.log(trap([2, 0, 2])); // 2
 // console.log(trap([4, 2, 3])); // 1
 // console.log(trap([5, 4, 1, 2])); // 1
-console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // 6
+// console.log(trap([2, 1, 0, 2])); // 3
+console.log(trap([5, 2, 1, 2, 1, 5])); // 14
