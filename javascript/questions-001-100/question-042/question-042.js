2e67d49444ab911c2c53e0700958e2a5cd1f8ac8
Dave Maynard <davemaynard24@gmail.com>
2020-04-20 10:37:41 -0400

    solved problem 42 with dynamic programming

--- javascript/questions-001-100/question-042/question-042.js
+++ javascript/questions-001-100/question-042/question-042.js
@@ -5,44 +5,28 @@
 const trap = function(height) {
   let amount = 0;
 
-  const slopes = [height[0]];
+  const leftMax = [height[0]];
   for (let i = 1; i < height.length; i++) {
-    slopes.push(height[i] - height[i - 1]);
+    leftMax[i] = Math.max(leftMax[i - 1], height[i]);
   }
 
-  console.log(slopes.join(`,`));
-
-  const peaks = [];
-  if (slopes[0] > slopes[1]) {
-    peaks.push(0);
-  }
-  for (let i = 1; i < slopes.length - 1; i++) {
-    if (slopes[i - 1] < slopes[i] && slopes[i] > slopes[i + 1]) {
-      peaks.push(i);
-    }
-  }
-  if (slopes[slopes.length - 2] < slopes[slopes.length - 1]) {
-    peaks.push(slopes.length - 1);
+  const rightMax = [];
+  rightMax[height.length - 1] = height[height.length - 1];
+  for (let i = height.length - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(rightMax[i + 1], height[i]);
   }
 
-  console.log(peaks);
-
-  for (let i = 0; i < peaks.length - 1; i++) {
-    const shortPeak = Math.min(height[peaks[i]], height[peaks[i + 1]]);
-    for (let j = peaks[i] + 1; j < peaks[i + 1]; j++) {
-      if (shortPeak - height[j] > 0) {
-        amount += shortPeak - height[j];
-      }
-    }
+  for (let i = 0; i < height.length; i++) {
+    amount += Math.min(leftMax[i], rightMax[i]) - height[i];
   }
 
   return amount;
 };
 
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // 6
-// console.log(trap([0, 1, 0, 2])); // 1
-// console.log(trap([2, 0, 2])); // 2
-// console.log(trap([4, 2, 3])); // 1
-// console.log(trap([5, 4, 1, 2])); // 1
-// console.log(trap([2, 1, 0, 2])); // 3
+console.log(trap([0, 1, 0, 2])); // 1
+console.log(trap([2, 0, 2])); // 2
+console.log(trap([4, 2, 3])); // 1
+console.log(trap([5, 4, 1, 2])); // 1
+console.log(trap([2, 1, 0, 2])); // 3
 console.log(trap([5, 2, 1, 2, 1, 5])); // 14
