fc0f84efff2c56afc3b069e8061af773adedb24f
Dave Maynard <davemaynard24@gmail.com>
2020-04-20 11:06:26 -0400

    optimized solution 42 with pointers

--- javascript/questions-001-100/question-042/question-042.js
+++ javascript/questions-001-100/question-042/question-042.js
@@ -3,21 +3,26 @@
  * @return {number}
  */
 const trap = function(height) {
+  if (height.length === 0) {
+    return 0;
+  }
+
   let amount = 0;
 
-  const leftMax = [height[0]];
-  for (let i = 1; i < height.length; i++) {
-    leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-  }
-
-  const rightMax = [];
-  rightMax[height.length - 1] = height[height.length - 1];
-  for (let i = height.length - 2; i >= 0; i--) {
-    rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-  }
-
-  for (let i = 0; i < height.length; i++) {
-    amount += Math.min(leftMax[i], rightMax[i]) - height[i];
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+  while (left < right) {
+    leftMax = Math.max(leftMax, height[left]);
+    rightMax = Math.max(rightMax, height[right]);
+    if (leftMax < rightMax) {
+      amount += leftMax - height[left];
+      left++;
+    } else {
+      amount += rightMax - height[right];
+      right--;
+    }
   }
 
   return amount;
